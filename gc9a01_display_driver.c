1d299fe69c131144e9630a253d6b49506845f0ad
tomashco <tommaso@ndg.ai>
2025-02-09 23:42:33 +0100

    fix: alignment with st7789_display_driver

--- gc9a01_display_driver.c
+++ gc9a01_display_driver.c
@@ -89,6 +89,11 @@ static const char *TAG = "gc9a01_display_driver";
 
 static void send_message(term pid, term message, GlobalContext *global);
 
+static inline void delay(int ms)
+{
+    vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
 struct SPI
 {
     struct SPIDisplay spi_disp;
@@ -155,16 +160,511 @@ static QueueHandle_t display_messages_queue;
 static NativeHandlerResult display_driver_consume_mailbox(Context *ctx);
 static void display_init(Context *ctx, term opts);
 
+static inline void writedata(struct SPI *spi, uint8_t data)
+{
+    spi_device_acquire_bus(spi->spi_disp.handle, portMAX_DELAY);
+    spi_display_write(&spi->spi_disp, 8, data);
+    spi_device_release_bus(spi->spi_disp.handle);
+}
+
 static inline void writecommand(struct SPI *spi, uint8_t cmd)
 {
     gpio_set_level(spi->dc_gpio, 0);
-    spi_display_write(&spi->spi_disp, 8, cmd);
+    writedata(spi, command);
     gpio_set_level(spi->dc_gpio, 1);
 }
 
-static inline void writedata(struct SPI *spi, uint8_t data)
+
+static inline void set_screen_paint_area(struct SPI *spi, int x, int y, int width, int height)
 {
-    spi_display_write(&spi->spi_disp, 8, data);
+    writecommand(spi, ST7789_CASET);
+    spi_device_acquire_bus(spi->spi_disp.handle, portMAX_DELAY);
+    spi_display_write(&spi->spi_disp, 32, (x << 16) | ((x + width) - 1));
+    spi_device_release_bus(spi->spi_disp.handle);
+
+    writecommand(spi, ST7789_RASET);
+    spi_device_acquire_bus(spi->spi_disp.handle, portMAX_DELAY);
+    spi_display_write(&spi->spi_disp, 32, (y << 16) | ((y + height) - 1));
+    spi_device_release_bus(spi->spi_disp.handle);
+}
+
+static int draw_image_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int y = item->y;
+
+    uint16_t bgcolor = 0;
+    bool visible_bg;
+    if (item->brcolor != 0) {
+        bgcolor = rgba8888_color_to_rgb565(screen, item->brcolor);
+        visible_bg = true;
+    } else {
+        visible_bg = false;
+    }
+
+    int width = item->width;
+    const char *data = item->data.image_data.pix;
+
+    int drawn_pixels = 0;
+
+    uint32_t *pixels = ((uint32_t *) data) + (ypos - y) * width + (xpos - x);
+    uint16_t *pixmem16 = (uint16_t *) (((uint8_t *) screen->pixels) + xpos * sizeof(uint16_t));
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        uint32_t img_pixel = READ_32_UNALIGNED(pixels);
+        uint8_t alpha = rgba8888_get_alpha(img_pixel);
+        if (alpha == 0xFF) {
+            uint16_t color = uint32_color_to_surface(screen, img_pixel);
+            pixmem16[drawn_pixels] = color;
+        } else if (visible_bg) {
+            uint16_t color = rgba8888_color_to_rgb565(screen, img_pixel);
+            uint16_t blended = alpha_blend_rgb565(color, bgcolor, alpha);
+            pixmem16[drawn_pixels] = rgb565_color_to_surface(screen, blended);
+        } else {
+            return drawn_pixels;
+        }
+        drawn_pixels++;
+        pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int draw_scaled_cropped_img_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int y = item->y;
+
+    uint16_t bgcolor = 0;
+    bool visible_bg;
+    if (item->brcolor != 0) {
+        bgcolor = rgba8888_color_to_rgb565(screen, item->brcolor);
+        visible_bg = true;
+    } else {
+        visible_bg = false;
+    }
+
+    int width = item->width;
+    const char *data = item->data.image_data_with_size.pix;
+
+    int drawn_pixels = 0;
+
+    int y_scale = item->y_scale;
+    int x_scale = item->x_scale;
+    int img_width = item->data.image_data_with_size.width;
+
+    int source_x = item->source_x;
+    int source_y = item->source_y;
+
+    uint32_t *pixels = ((uint32_t *) data) + (source_y + ((ypos - y) / y_scale)) * img_width + source_x + ((xpos - x) / x_scale);
+    uint16_t *pixmem16 = (uint16_t *) (((uint8_t *) screen->pixels) + xpos * sizeof(uint16_t));
+
+    if (source_x + (width / x_scale) > img_width) {
+        width = (img_width - source_x) * x_scale;
+    }
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        uint32_t img_pixel = READ_32_UNALIGNED(pixels);
+        uint8_t alpha = rgba8888_get_alpha(img_pixel);
+        if (alpha == 0xFF) {
+            uint16_t color = uint32_color_to_surface(screen, img_pixel);
+            pixmem16[drawn_pixels] = color;
+        } else if (visible_bg) {
+            uint16_t color = rgba8888_color_to_rgb565(screen, img_pixel);
+            uint16_t blended = alpha_blend_rgb565(color, bgcolor, alpha);
+            pixmem16[drawn_pixels] = rgb565_color_to_surface(screen, blended);
+        } else {
+            return drawn_pixels;
+        }
+        drawn_pixels++;
+        // TODO: optimize here
+        pixels = ((uint32_t *) data) + (source_y + ((ypos - y) / y_scale)) * img_width + source_x + (j / x_scale);
+    }
+
+    return drawn_pixels;
+}
+
+static int draw_rect_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int width = item->width;
+    uint16_t color = uint32_color_to_surface(screen, item->brcolor);
+
+    int drawn_pixels = 0;
+
+    uint16_t *pixmem16 = (uint16_t *) (((uint8_t *) screen->pixels) + xpos * sizeof(uint16_t));
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        pixmem16[drawn_pixels] = color;
+        drawn_pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int draw_text_x(int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+{
+    int x = item->x;
+    int y = item->y;
+    uint16_t fgcolor = uint32_color_to_surface(screen, item->data.text_data.fgcolor);
+    uint16_t bgcolor;
+    bool visible_bg;
+    if (item->brcolor != 0) {
+        bgcolor = uint32_color_to_surface(screen, item->brcolor);
+        visible_bg = true;
+    } else {
+        visible_bg = false;
+    }
+
+    char *text = (char *) item->data.text_data.text;
+
+    int width = item->width;
+
+    int drawn_pixels = 0;
+
+    uint16_t *pixmem32 = (uint16_t *) (((uint8_t *) screen->pixels) + xpos * sizeof(uint16_t));
+
+    if (width > xpos - x + max_line_len) {
+        width = xpos - x + max_line_len;
+    }
+
+    for (int j = xpos - x; j < width; j++) {
+        int char_index = j / CHAR_WIDTH;
+        char c = text[char_index];
+        unsigned const char *glyph = fontdata + ((unsigned char) c) * 16;
+
+        unsigned char row = glyph[ypos - y];
+
+        bool opaque;
+        int k = j % CHAR_WIDTH;
+        if (row & (1 << (7 - k))) {
+            opaque = true;
+        } else {
+            opaque = false;
+        }
+
+        if (opaque) {
+            pixmem32[drawn_pixels] = fgcolor;
+        } else if (visible_bg) {
+            pixmem32[drawn_pixels] = bgcolor;
+        } else {
+            return drawn_pixels;
+        }
+        drawn_pixels++;
+    }
+
+    return drawn_pixels;
+}
+
+static int find_max_line_len(BaseDisplayItem *items, int count, int xpos, int ypos)
+{
+    int line_len = screen->w;
+
+    for (int i = 0; i < count; i++) {
+        BaseDisplayItem *item = &items[i];
+
+        if ((xpos < item->x) && (ypos >= item->y) && (ypos < item->y + item->height)) {
+            int len_to_item = item->x - xpos;
+            line_len = (line_len > len_to_item) ? len_to_item : line_len;
+        }
+    }
+
+    return line_len;
+}
+
+static int draw_x(int xpos, int ypos, BaseDisplayItem *items, int items_count)
+{
+    bool below = false;
+
+    for (int i = 0; i < items_count; i++) {
+        BaseDisplayItem *item = &items[i];
+        if ((xpos < item->x) || (xpos >= item->x + item->width) || (ypos < item->y) || (ypos >= item->y + item->height)) {
+            continue;
+        }
+
+        int max_line_len = below ? 1 : find_max_line_len(items, i, xpos, ypos);
+
+        int drawn_pixels = 0;
+        switch (items[i].primitive) {
+            case Image:
+                drawn_pixels = draw_image_x(xpos, ypos, max_line_len, item);
+                break;
+
+            case Rect:
+                drawn_pixels = draw_rect_x(xpos, ypos, max_line_len, item);
+                break;
+
+            case ScaledCroppedImage:
+                drawn_pixels = draw_scaled_cropped_img_x(xpos, ypos, max_line_len, item);
+                break;
+
+            case Text:
+                drawn_pixels = draw_text_x(xpos, ypos, max_line_len, item);
+                break;
+            default: {
+                fprintf(stderr, "unexpected display list command.\n");
+            }
+        }
+
+        if (drawn_pixels != 0) {
+            return drawn_pixels;
+        }
+
+        below = true;
+    }
+
+    return 1;
+}
+
+static void do_update(Context *ctx, term display_list)
+{
+    int proper;
+    int len = term_list_length(display_list, &proper);
+
+    BaseDisplayItem *items = malloc(sizeof(BaseDisplayItem) * len);
+
+    term t = display_list;
+    for (int i = 0; i < len; i++) {
+        init_item(&items[i], term_get_list_head(t), ctx);
+        t = term_get_list_tail(t);
+    }
+
+    int screen_width = screen->w;
+    int screen_height = screen->h;
+    struct SPI *spi = ctx->platform_data;
+
+    set_screen_paint_area(spi, 0, 0, screen_width, screen_height);
+    writecommand(spi, ST7789_RAMWR);
+    spi_device_acquire_bus(spi->spi_disp.handle, portMAX_DELAY);
+
+    bool transaction_in_progress = false;
+
+    for (int ypos = 0; ypos < screen_height; ypos++) {
+        int xpos = 0;
+        while (xpos < screen_width) {
+            int drawn_pixels = draw_x(xpos, ypos, items, len);
+            xpos += drawn_pixels;
+        }
+
+        if (transaction_in_progress) {
+            spi_transaction_t *trans;
+            // I did a quick measurement, and most of the time is spent waiting for DMA transaction
+            // eg. 23 us spent in draw_x, 188 us spent in spi_device_get_trans_result
+            spi_device_get_trans_result(spi->spi_disp.handle, &trans, portMAX_DELAY);
+        }
+
+        // NEW CODE
+        void *tmp = screen->pixels;
+        screen->pixels = screen->pixels_out;
+        screen->pixels_out = tmp;
+        spi_display_dmawrite(&spi->spi_disp, screen_width * sizeof(uint16_t), screen->pixels_out);
+        transaction_in_progress = true;
+    }
+
+    if (transaction_in_progress) {
+        spi_transaction_t *trans;
+        spi_device_get_trans_result(spi->spi_disp.handle, &trans, portMAX_DELAY);
+    }
+
+    spi_device_release_bus(spi->spi_disp.handle);
+
+    destroy_items(items, len);
+}
+
+static void draw_buffer(struct SPI *spi, int x, int y, int width, int height, const void *imgdata)
+{
+    const uint16_t *data = imgdata;
+
+    set_screen_paint_area(spi, x, y, width, height);
+
+    writecommand(spi, ST7789_RAMWR);
+
+    int dest_size = width * height;
+    int buf_pixel_size = (dest_size > 1024) ? 1024 : dest_size;
+
+    int chunks = dest_size / 1024;
+
+    uint16_t *tmpbuf = heap_caps_malloc(buf_pixel_size * sizeof(uint16_t), MALLOC_CAP_DMA);
+
+    spi_device_acquire_bus(spi->spi_disp.handle, portMAX_DELAY);
+    for (int i = 0; i < chunks; i++) {
+        const uint16_t *data_b = data + 1024 * i;
+        for (int j = 0; j < 1024; j++) {
+            tmpbuf[j] = SPI_SWAP_DATA_TX(data_b[j], 16);
+        }
+        spi_display_dmawrite(&spi->spi_disp, buf_pixel_size * sizeof(uint16_t), tmpbuf);
+    }
+    int last_chunk_size = dest_size - chunks * 1024;
+    if (last_chunk_size) {
+        const uint16_t *data_b = data + chunks * 1024;
+        for (int j = 0; j < 1024; j++) {
+            tmpbuf[j] = SPI_SWAP_DATA_TX(data_b[j], 16);
+        }
+        spi_display_dmawrite(&spi->spi_disp, last_chunk_size * sizeof(uint16_t), tmpbuf);
+    }
+    spi_device_release_bus(spi->spi_disp.handle);
+
+    free(tmpbuf);
+}
+
+
+static void process_message(Message *message, Context *ctx)
+{
+    GenMessage gen_message;
+    if (UNLIKELY(port_parse_gen_message(message->message, &gen_message) != GenCallMessage)) {
+        fprintf(stderr, "Received invalid message.");
+        AVM_ABORT();
+    }
+
+    term req = gen_message.req;
+    if (UNLIKELY(!term_is_tuple(req) || term_get_tuple_arity(req) < 1)) {
+        AVM_ABORT();
+    }
+    term cmd = term_get_tuple_element(req, 0);
+
+    struct SPI *spi = ctx->platform_data;
+
+    if (cmd == context_make_atom(ctx, "\x6"
+                                      "update")) {
+        term display_list = term_get_tuple_element(req, 1);
+        do_update(ctx, display_list);
+
+    } else if (cmd == context_make_atom(ctx, "\xB"
+                                             "draw_buffer")) {
+        int x = term_to_int(term_get_tuple_element(req, 1));
+        int y = term_to_int(term_get_tuple_element(req, 2));
+        int width = term_to_int(term_get_tuple_element(req, 3));
+        int height = term_to_int(term_get_tuple_element(req, 4));
+        unsigned long addr_low = term_to_int(term_get_tuple_element(req, 5));
+        unsigned long addr_high = term_to_int(term_get_tuple_element(req, 6));
+
+        const void *data = (const void *) ((addr_low | (addr_high << 16)));
+
+        draw_buffer(spi, x, y, width, height, data);
+
+        // draw_buffer is a kind of cast, no need to reply
+        return;
+
+    } else {
+        fprintf(stderr, "display: ");
+        term_display(stderr, req, ctx);
+        fprintf(stderr, "\n");
+    }
+
+    BEGIN_WITH_STACK_HEAP(TUPLE_SIZE(2) + REF_SIZE, heap);
+    term return_tuple = term_alloc_tuple(2, &heap);
+    term_put_tuple_element(return_tuple, 0, gen_message.ref);
+    term_put_tuple_element(return_tuple, 1, OK_ATOM);
+
+    send_message(gen_message.pid, return_tuple, ctx->global);
+    END_WITH_STACK_HEAP(heap, ctx->global);
+}
+
+
+static void process_messages(void *arg)
+{
+    struct SPI *args = arg;
+
+    while (true) {
+        Message *message;
+        xQueueReceive(display_messages_queue, &message, portMAX_DELAY);
+        process_message(message, args->ctx);
+
+        BEGIN_WITH_STACK_HEAP(1, temp_heap);
+        mailbox_message_dispose(&message->base, &temp_heap);
+        END_WITH_STACK_HEAP(temp_heap, args->ctx->global);
+    }
+}
+
+static NativeHandlerResult display_driver_consume_mailbox(Context *ctx)
+{
+    MailboxMessage *mbox_msg = mailbox_take_message(&ctx->mailbox);
+    Message *msg = CONTAINER_OF(mbox_msg, Message, base);
+
+    xQueueSend(display_messages_queue, &msg, 1);
+
+    return NativeContinue;
+}
+
+static void set_rotation(struct SPI *spi, int rotation)
+{
+    if (rotation == 1) {
+        writecommand(spi, GC9A01_MADCTL);
+        writedata(spi, TFT_MAD_COLOR_ORDER);
+    }
+}
+
+Context *gc9a01_display_create_port(GlobalContext *global, term opts)
+{
+    Context *ctx = context_new(global);
+    ctx->native_handler = display_driver_consume_mailbox;
+    display_init(ctx, opts);
+    return ctx;
+} 
+
+static void send_message(term pid, term message, GlobalContext *global)
+{
+    int local_process_id = term_to_local_process_id(pid);
+    globalcontext_send_message(global, local_process_id, message);
+}
+
+
+static void display_init(Context *ctx, term opts)
+{
+    struct SPI *spi = malloc(sizeof(struct SPI));
+    if (!spi) {
+        ESP_LOGE(TAG, "Failed to allocate SPI structure");
+        return;
+    }
+
+    struct SPIDisplayConfig spi_config;
+    spi_display_init_config(&spi_config);
+    spi_config.clock_speed_hz = SPI_CLOCK_HZ;
+    spi_config.mode = SPI_MODE;
+    spi_display_parse_config(&spi_config, opts, ctx->global);
+    spi_display_init(&spi->spi_disp, &spi_config);
+
+    bool ok = display_common_gpio_from_opts(
+        opts, ATOM_STR("\x2", "dc"), &spi->dc_gpio, ctx->global);
+    ok = ok && display_common_gpio_from_opts(
+        opts, ATOM_STR("\x5", "reset"), &spi->reset_gpio, ctx->global);
+
+    if (!ok) {
+        ESP_LOGE(TAG, "Failed init: invalid GPIO configuration");
+        free(spi);
+        return;
+    }
+
+    // Reset
+    gpio_set_direction(spi->reset_gpio, GPIO_MODE_OUTPUT);
+    gpio_set_level(spi->reset_gpio, 1);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+    gpio_set_level(spi->reset_gpio, 0);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+    gpio_set_level(spi->reset_gpio, 1);
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+
+    gpio_set_direction(spi->dc_gpio, GPIO_MODE_OUTPUT);
+
+    display_init_gc9a01(spi);
+
+    ctx->platform_data = spi;
+    spi->ctx = ctx;
+
+    display_messages_queue = xQueueCreate(32, sizeof(Message *));
+    xTaskCreate(process_messages, "display", 10000, spi, 1, NULL);
 }
 
 static void display_init_gc9a01(struct SPI *spi)
@@ -329,57 +829,3 @@ static void display_init_gc9a01(struct SPI *spi)
     writecommand(spi, GC9A01_DISPON);
     vTaskDelay(20 / portTICK_PERIOD_MS);
 }
-
-static void display_init(Context *ctx, term opts)
-{
-    struct SPI *spi = malloc(sizeof(struct SPI));
-    if (!spi) {
-        ESP_LOGE(TAG, "Failed to allocate SPI structure");
-        return;
-    }
-
-    struct SPIDisplayConfig spi_config;
-    spi_display_init_config(&spi_config);
-    spi_config.clock_speed_hz = SPI_CLOCK_HZ;
-    spi_config.mode = SPI_MODE;
-    spi_display_parse_config(&spi_config, opts, ctx->global);
-    spi_display_init(&spi->spi_disp, &spi_config);
-
-    bool ok = display_common_gpio_from_opts(
-        opts, ATOM_STR("\x2", "dc"), &spi->dc_gpio, ctx->global);
-    ok = ok && display_common_gpio_from_opts(
-        opts, ATOM_STR("\x5", "reset"), &spi->reset_gpio, ctx->global);
-
-    if (!ok) {
-        ESP_LOGE(TAG, "Failed init: invalid GPIO configuration");
-        free(spi);
-        return;
-    }
-
-    // Reset
-    gpio_set_direction(spi->reset_gpio, GPIO_MODE_OUTPUT);
-    gpio_set_level(spi->reset_gpio, 1);
-    vTaskDelay(50 / portTICK_PERIOD_MS);
-    gpio_set_level(spi->reset_gpio, 0);
-    vTaskDelay(50 / portTICK_PERIOD_MS);
-    gpio_set_level(spi->reset_gpio, 1);
-    vTaskDelay(50 / portTICK_PERIOD_MS);
-
-    gpio_set_direction(spi->dc_gpio, GPIO_MODE_OUTPUT);
-
-    display_init_gc9a01(spi);
-
-    ctx->platform_data = spi;
-    spi->ctx = ctx;
-
-    display_messages_queue = xQueueCreate(32, sizeof(Message *));
-    xTaskCreate(process_messages, "display", 10000, spi, 1, NULL);
-}
-
-Context *gc9a01_display_create_port(GlobalContext *global, term opts)
-{
-    Context *ctx = context_new(global);
-    ctx->native_handler = display_driver_consume_mailbox;
-    display_init(ctx, opts);
-    return ctx;
-} 
